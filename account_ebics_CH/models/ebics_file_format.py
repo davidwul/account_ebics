4c4e0368a8f8708976453d77fc9c21c4c2bc00cc
David Wulliamoz <dwulliamoz@compassion.ch>
2020-10-30 01:13:49 +0100

    corr dumb!

--- account_ebics_CH/models/ebics_file_format.py
+++ account_ebics_CH/models/ebics_file_format.py
@@ -9,7 +9,7 @@ class EbicsFileFormat(models.Model):
     _inherit = 'ebics.file.format'
 
     def _supported_download_order_types(self):
-        res = super()
+        res = super()._supported_download_order_types()
         res.append("ZZT")
         res.append("ZZQ")
         return res
