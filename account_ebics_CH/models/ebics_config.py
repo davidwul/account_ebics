171404cae4c7441fa0c71b90ef15099dc44c73bc
David Wulliamoz <dwulliamoz@compassion.ch>
2020-10-30 01:34:50 +0100

    allow changing file format when config in done state

---
+++ account_ebics_CH/models/ebics_config.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Compassion.
+# License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
+
+from odoo import api, models, _
+from odoo.exceptions import UserError
+
+
+class EbicsConfig(models.Model):
+    _inherit = 'ebics.config'
+
+    ebics_file_format_ids = fields.Many2many(
+        comodel_name='ebics.file.format',
+        column1='config_id', column2='format_id',
+        string='EBICS File Formats',
+    )
